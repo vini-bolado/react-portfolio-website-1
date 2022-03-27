0c39051cfcc35f8fa2d220fc2e9fdb907c38423b
vinicius evandro <vinnievandro@hotmail.com>
2022-03-27 20:02:33 -0300

    serviços

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,131 @@
 import React from 'react'
 import './services.css'
+import {AiOutlineCheck} from 'react-icons/ai'
 
 const Services = () => {
   return (
-    <section id='services'>serviços</section>
+    <section id='services'>
+      <h5>What i offering</h5>
+      <h2>services</h2>
+
+    <div className='container services__container'>
+      <article className='service'>
+        <div className="service__head">
+          <h3>UI/UX</h3>
+        </div>
+
+
+        <ul className='service__list'>
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+        </ul>
+      </article>
+
+{/* END OF UI/UX */}
+
+      <article className='service'>
+        <div className="service__head">
+          <h3>UI/UX</h3>
+        </div>
+
+
+        <ul className='service__list'>
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+        </ul>
+      </article>
+
+      {/* END OF DEV WEB */}
+
+
+      <article className='service'>
+        <div className="service__head">
+          <h3>UI/UX</h3>
+        </div>
+
+
+        <ul className='service__list'>
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+
+          <li>
+            <AiOutlineCheck className='service__list-icon'/>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </li>
+        </ul>
+      </article>
+
+      {/* END OF UI/UX */}
+
+    </div>
+
+
+
+    </section>
   )
 }
 
