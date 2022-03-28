301197100881c65ea4a4c8f57aee64e99dbd0d06
vinicius evandro <vinnievandro@hotmail.com>
2022-03-28 17:00:01 -0300

    port final

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,91 @@
 import './portfolio.css'
 import React from 'react'
+import EU from '../../assets/me.png'
+
+// Usando método
+
+const data = [
+  {
+    id: 1,
+    image: EU,
+    title: 'Roubando fazendo isCursorAtStart',
+    github: 'https://github.com/vini-bolado?tab=repositories',
+    demo: 'https://github.com/vini-bolado?tab=repositories'
+  },
+  {
+    id: 2,
+    image: EU,
+    title: 'Roubando fazendo isCursorAtStart',
+    github: 'https://github.com/vini-bolado?tab=repositories',
+    demo: 'https://github.com/vini-bolado?tab=repositories'
+  },
+  {
+    id: 3,
+    image: EU,
+    title: 'Roubando fazendo isCursorAtStart',
+    github: 'https://github.com/vini-bolado?tab=repositories',
+    demo: 'https://github.com/vini-bolado?tab=repositories'
+  },
+  {
+    id: 4,
+    image: EU,
+    title: 'Roubando fazendo isCursorAtStart',
+    github: 'https://github.com/vini-bolado?tab=repositories',
+    demo: 'https://github.com/vini-bolado?tab=repositories'
+  },
+  {
+    id: 5,
+    image: EU,
+    title: 'Roubando fazendo isCursorAtStart',
+    github: 'https://github.com/vini-bolado?tab=repositories',
+    demo: 'https://github.com/vini-bolado?tab=repositories'
+  },
+  {
+    id: 6,
+    image: EU,
+    title: 'Roubando fazendo isCursorAtStart',
+    github: 'https://github.com/vini-bolado?tab=repositories',
+    demo: 'https://github.com/vini-bolado?tab=repositories'
+  },
+]
+
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+
+      <h5>Trabalhos recentes</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+
+      {
+        data.map(({id,image,title,github,demo}) =>{
+          return (
+            <article key={id} className='portfolio__item'>
+            <div className="portfolio__item-image"></div>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+            <a href={github} className='btn'>git</a>
+            <a href={demo} className='btn btn-primary'>git lab</a>
+            </div>
+          </article>
+          )
+
+        })
+      
+          
+     
+        
+      }
+      
+
+
+      
+      </div>
+
+    </section>
   )
 }
 
