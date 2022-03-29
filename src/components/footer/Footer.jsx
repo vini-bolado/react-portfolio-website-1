38369297e4fe9ee36c882cd552e3edadafdfc48c
vinicius evandro <vinnievandro@hotmail.com>
2022-03-29 16:38:44 -0300

    Finalizade sem refinamento

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,34 @@
 import './footer.css'
-
 import React from 'react'
+import {BsInstagram} from 'react-icons/bs'
+
 
 const Footer = () => {
   return (
-    <section id=''>About</section>
+    <footer>
+      <a href="#" className='footer__logo' >Bolado LTDA</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="hhtps://facebook.com"><BsInstagram/></a>
+        <a href="hhtps://facebook.com"><BsInstagram/></a>
+        <a href="hhtps://facebook.com"><BsInstagram/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Bolado LTDA Todos os direitos reservados.</small>
+      </div>
+
+    </footer>
   )
 }
 
